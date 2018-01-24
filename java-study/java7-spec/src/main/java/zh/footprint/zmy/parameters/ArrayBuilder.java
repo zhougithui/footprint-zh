71a418eb951911e001556f059efcdbd912f4d9f8
周辉 <hui.zhou@lanmaoly.com>
2018-01-24 13:31:56 +0800

    java7

---
+++ java-study/java7-spec/src/main/java/zh/footprint/zmy/parameters/ArrayBuilder.java
@@ -0,0 +1,37 @@
+package zh.footprint.zmy.parameters;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 可变参数警告
+ */
+public class ArrayBuilder {
+
+    public static <T> void addToList(List<T> listArg, T... elements) {
+        for (T x : elements) {
+            listArg.add(x);
+        }
+    }
+
+    @SuppressWarnings({"unchecked", "varargs"})
+    public static <T> void addToList2(List<T> listArg, T... elements) {
+        for (T x : elements) {
+            listArg.add(x);
+        }
+    }
+
+    @SafeVarargs
+    public static <T> void addToList3(List<T> listArg, T... elements) {
+        for (T x : elements) {
+            listArg.add(x);
+        }
+    }
+
+    public static void faultyMethod(List<String>... l) {
+        Object[] objectArray = l;  // Valid
+        objectArray[0] = Arrays.asList(new Integer(42));
+        String s = l[0].get(0);    // ClassCastException thrown here
+    }
+
+}
